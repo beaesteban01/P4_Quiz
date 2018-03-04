2663489763fd89de8b79c6f87245768e8419bd7d
beaesteban01 <36420425+beaesteban01@users.noreply.github.com>
2018-03-04 12:36:51 +0100

    Update cmds.js
    Cambio el prompt de sitio

--- cmds.js
+++ cmds.js
@@ -127,9 +127,10 @@ exports.testCmd = (rl,id) => {
 			   	} else {
 			   		log("Su respuesta es incorrecta");
 			   		biglog('INCORRECTO', 'red');
+					rl.prompt();
 			   	}
 
-			   	rl.prompt();
+			   	
 			   });
 
 		} catch(error){
